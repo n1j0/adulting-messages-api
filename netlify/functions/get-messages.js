44a6b4295b4ab16f89051df7b798c2d4a1768d4d
Jonas Pfannkuche <jonas.pfannkuche@hdnet.de>
2022-08-07 18:33:09 +0200

    feat(db): add first attempt to get data

--- netlify/functions/get-messages.js
+++ netlify/functions/get-messages.js
@@ -1,16 +1,11 @@
 exports.handler = async function (event, context) {
   require('dotenv').config();
-  const mysql = require('mysql2');
-  const connection = mysql.createConnection(process.env.DATABASE_URL);
-  console.log('Connected to PlanetScale!');
+  const mysql = require('mysql2/promise');
+  const connection = await mysql.createConnection(process.env.DATABASE_URL);
+  const [rows, fields] = await connection.execute("SELECT * FROM 'messages'");
   connection.end();
   return {
     statusCode: 200,
-    body: JSON.stringify([
-      {
-        text: 'Test',
-        date: '',
-      }
-    ]),
+    body: JSON.stringify(rows),
   };
 };
