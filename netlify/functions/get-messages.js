5db6a9289833e09c2675e06a901e068eafc30ff3
Jonas Pfannkuche <jonas.pfannkuche@hdnet.de>
2022-08-07 18:40:41 +0200

    fix(function): replace ticks

--- netlify/functions/get-messages.js
+++ netlify/functions/get-messages.js
@@ -2,7 +2,7 @@ exports.handler = async function (event, context) {
   require('dotenv').config();
   const mysql = require('mysql2/promise');
   const connection = await mysql.createConnection(process.env.DATABASE_URL);
-  const [rows, fields] = await connection.execute('SELECT * FROM "messages"');
+  const [rows, fields] = await connection.execute('SELECT * FROM `messages`');
   connection.end();
   return {
     statusCode: 200,
